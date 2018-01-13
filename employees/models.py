b5a2a1a99e2fad9d22932188e5b63df76b31ae35
Giovanni Cornachini <giovannicornachini@gmail.com>
2018-01-13 12:02:04 -0200

    Department and Employee models created; Test coverage

--- employees/models.py
+++ employees/models.py
@@ -3,4 +3,12 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Employee(models.Model):
+    name = models.CharField(max_length=150)
+    email = models.EmailField(unique=True)
+    department = models.ForeignKey('employees.Department')
+
+
+class Department(models.Model):
+    name = models.CharField(max_length=50, unique=True)
--- employees/tests.py
+++ employees/tests.py
@@ -2,5 +2,22 @@
 from __future__ import unicode_literals
 
 from django.test import TestCase
+from django.db import IntegrityError
 
-# Create your tests here.
+from employees.models import Employee, Department
+
+
+class EmployeeTestCase(TestCase):
+    def setUp(self):
+        self.marketing = Department.objects.create(name='Marketing')
+        self.finance = Department.objects.create(name='Finance')
+
+        self.employee = Employee.objects.create(name='José', email='jose@gmail.com',
+                                                department=self.marketing)
+
+    def test_empregados_nao_podem_pertencer_a_dois_departamentos(self):
+        """ Um funcionario não deve pertencer a mais de um departamento. """
+        self.assertEqual(self.employee.department, self.marketing)
+
+        with self.assertRaises(IntegrityError):
+            Employee.objects.create(name='José', email='jose@gmail.com', department=self.finance)
