b49cfe1cf49fe338b64e8ebc77b2351a672ad12f
Giovanni Cornachini <giovannicornachini@gmail.com>
2018-01-13 18:43:57 -0200

    Admin update

--- employees/admin.py
+++ employees/admin.py
@@ -3,4 +3,21 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-# Register your models here.
+from employees.models import Employee, Department
+
+
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'department', 'email')
+
+
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'employees_count')
+
+    def employees_count(self, obj):
+        """ Quantidade de employees por departamento. """
+        return obj.employees_count()
+    employees_count.short_description = 'Employees Count'
+
+
+admin.site.register(Employee, EmployeeAdmin)
+admin.site.register(Department, DepartmentAdmin)
